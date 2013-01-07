fc701289bb614efc401fb88d4af8553d7bc02ec7
Chris Kimpton <chris@kimptoc.net>
2013-01-07 21:03:09 +0000

    update checks

--- hourly_checks.sh
+++ hourly_checks.sh
@@ -6,9 +6,13 @@ touch last_run_time.txt
 SUPPORTEMAIL=chris@kimptoc.net
 
 
+
 sh check_url.sh http://home.kimptoc.net:8080/ chris.kimpton@rabobank.com
 sh check_url.sh http://home.kimptoc.net:8080/ chris@kimptoc.net
 sh check_url.sh http://tagadab.kimptoc.net/ chris@kimptoc.net
 
-sh check_url.sh http://www.stjoanofarc.co.uk/ chris@kimptoc.net
+sh check_url.sh http://wowactivity.kimptoc.net/ chris@kimptoc.net
+
+sh check_url.sh http://hummingnow.com/ chris@kimptoc.net
+
 sh check_url.sh http://www.richleememorial.com/ chris@kimptoc.net
