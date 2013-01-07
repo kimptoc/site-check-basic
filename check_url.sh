55c6a402b7d2a7253839eb779f34293b1efa48d8
Chris Kimpton <chris@kimptoc.net>
2013-01-07 20:57:09 +0000

    init

---
+++ check_url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -v
+# $Id: check_url.sh,v 1.3 2002/03/23 23:59:42 admin Exp $
+# checks the specifed (1st param) url works, if not sends email to address of second param
+
+URLTOTEST=$1
+SUPPORTEMAIL=$2
+
+rm -fr WGET_TEMP
+wget --tries=2 --timeout=20 -q -PWGET_TEMP $URLTOTEST 2>&1 >/dev/null
+
+result=$?
+
+if [ "$result" -ne "0" ]; then
+        echo "Oh-oh - url not found! $URLTOTEST, sending email to $SUPPORTEMAIL"
+	echo "To: $SUPPORTEMAIL \n Date `date`:: Error checking for url : $URLTOTEST :: THIS SEEMS TO BE DOWN" | mail -s "Error checking for url : $URLTOTEST" $SUPPORTEMAIL
+fi
