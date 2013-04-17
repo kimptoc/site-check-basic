68c88f9672bd0a6039ba31048a0bbd4fccc8938d
Chris Kimpton <chris@kimptoc.net>
2013-04-17 19:34:04 +0100

    one less server

--- hourly_checks.sh
+++ hourly_checks.sh
@@ -18,7 +18,7 @@ sh check_url.sh http://home.kimptoc.net:8080/ chris.kimpton@rabobank.com
 sh check_url.sh http://home.kimptoc.net:8080/ chris@kimptoc.net
 
 #sh check_url.sh http://tagadab.kimptoc.net/ chris@kimptoc.net
-sh check_url.sh http://stormvz1.kimptoc.net/ chris@kimptoc.net
+#sh check_url.sh http://stormvz1.kimptoc.net/ chris@kimptoc.net
 sh check_url.sh http://liquid1.kimptoc.net/ chris@kimptoc.net
 #sh check_url.sh http://liquid2.kimptoc.net/ chris@kimptoc.net
 sh check_url.sh http://allyourvps1.kimptoc.net/ chris@kimptoc.net
