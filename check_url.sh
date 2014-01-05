0f5e09d05f31330775140d877622ed9089b6a1cc
Chris Kimpton <chris@kimptoc.net>
2014-01-05 14:18:46 +0000

    longer timeout, more info in email

--- check_url.sh
+++ check_url.sh
@@ -5,12 +5,14 @@
 URLTOTEST=$1
 SUPPORTEMAIL=$2
 
+starttime=`date`
+
 rm -fr WGET_TEMP
-wget --tries=2 --timeout=20 -q -PWGET_TEMP $URLTOTEST 2>&1 >/dev/null
+wget --tries=2 --timeout=40 -q -PWGET_TEMP $URLTOTEST 2>&1 >/dev/null
 
 result=$?
 
 if [ "$result" -ne "0" ]; then
         echo "Oh-oh - url not found! $URLTOTEST, sending email to $SUPPORTEMAIL"
-	echo "To: $SUPPORTEMAIL \n Date `date`:: Error checking for url : $URLTOTEST :: THIS SEEMS TO BE DOWN" | mail -s "Error checking for url : $URLTOTEST" $SUPPORTEMAIL
+	echo "To: $SUPPORTEMAIL \n Date `date`:: Error checking for url : $URLTOTEST :: THIS SEEMS TO BE DOWN \n Result code: $result \n Start time: $starttime" | mail -s "Error checking for url : $URLTOTEST" $SUPPORTEMAIL
 fi
