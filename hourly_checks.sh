a3b9285240a5f03f0514a19d54b25ec211bb4370
Chris Kimpton <chris@kimptoc.net>
2022-09-11 18:30:49 +0100

    updated docs, tweak hosts

--- hourly_checks.sh
+++ hourly_checks.sh
@@ -1,10 +1,11 @@
 cd ~/jobs/site-check-basic/
 
 # setup
-#  sudo service sendmail stop
-#  sudo apt-get remove sendmail
-#  sudo apt-get install postfix -- setup for internet host
-#  sudo vi /etc/postfix/main.cf -- ensure domain names correct
+#  sudo service sendmail stop  # not installed on ubuntu 18.04 , so ok
+#  sudo apt-get remove sendmail # not installed on ubuntu 18.04 , so ok
+######OLD####  sudo apt-get install postfix -- setup for internet host
+# sudo apt-get install mailutils 
+#  sudo vi /etc/postfix/main.cf -- ensure domain names correct, only using ipv4 (for now)
 #  sudo service postfix start
 
 touch last_run_time.txt
@@ -25,8 +26,8 @@ sh check_url.sh http://home.kimptoc.net:8080/ chris@kimptoc.net
 sh check_url.sh http://overzold1.kimptoc.net/ chris@kimptoc.net
 #sh check_url.sh http://waveride1.kimptoc.net/ chris@kimptoc.net
 #sh check_url.sh http://getkvm1.kimptoc.net/ chris@kimptoc.net
-#sh check_url.sh http://rhs1.kimptoc.net/ chris@kimptoc.net #docker box no apache
-sh check_url.sh http://ssdvps1.kimptoc.net/ chris@kimptoc.net
+sh check_url.sh http://rhs1.kimptoc.net/ chris@kimptoc.net #docker box no apache
+#sh check_url.sh http://ssdvps1.kimptoc.net/ chris@kimptoc.net
 
 #sh check_url.sh http://wowactivity.kimptoc.net/ chris@kimptoc.net
 
